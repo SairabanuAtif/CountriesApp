e4d8e785fcde02097bf8bc30c9c8996dff42cd69
Sairabanu Atif <sairabanumohd@gmail.com>
2025-09-22 10:11:35 +0530

    Fix region menu

--- app/src/main/java/com/example/countriesapp/ui/list/CountryListScreen.kt
+++ app/src/main/java/com/example/countriesapp/ui/list/CountryListScreen.kt
@@ -4,6 +4,7 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.gestures.detectTapGestures
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -39,6 +40,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextOverflow
@@ -126,7 +128,15 @@ fun CountryListScreen(
             )
 
             is UiState.Success ->
-                Box(modifier = Modifier.fillMaxSize()) {
+                Box(modifier = Modifier
+                    .fillMaxSize()
+                    .pointerInput(showRegionList) {
+                        detectTapGestures {
+                            if (showRegionList) {
+                                showRegionList = false
+                            }
+                        }
+                    }) {
                     Column(
                         modifier = Modifier
                             .fillMaxSize()
